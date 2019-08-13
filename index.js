c26234c1aed8c4b7a007b81d0a4291bbc1f6bd8e
tatiana.mojica <tatiana.mojica@bustle.com>
2019-08-13 01:03:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+let companyName = "Scuber";
+// const companyName = "";
+
+const mostProfitableNeighborhood = "Chelsea";
+// does not throw error
+
+const companyCeo = "Susan Smith"
